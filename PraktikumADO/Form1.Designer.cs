afe79b912f2f0e6008e9e3e5462c5df775c599e6
AkmalMaulana55 <vylingsmall55@gmail.com>
2026-03-12 20:07:26 +0700

    menambahkan try dan catch pada button hitungmk

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -65,6 +65,7 @@
             this.btnHitungMK.TabIndex = 2;
             this.btnHitungMK.Text = "HitungMK";
             this.btnHitungMK.UseVisualStyleBackColor = true;
+            this.btnHitungMK.Click += new System.EventHandler(this.btnHitungMK_Click);
             // 
             // btnUpdate
             // 
